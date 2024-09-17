123286fb72c845dc60d6941c9c9bc16e3a903b64
bidetofevil <hanson.ho@embrace.io>
2024-09-17 10:38:58 -0700

    Sample API snippet

--- demo-app/src/main/java/io/opentelemetry/android/demo/OtelDemoApplication.kt
+++ demo-app/src/main/java/io/opentelemetry/android/demo/OtelDemoApplication.kt
@@ -10,7 +10,6 @@ import android.app.Application
 import android.util.Log
 import io.opentelemetry.android.OpenTelemetryRum
 import io.opentelemetry.android.OpenTelemetryRumBuilder
-import io.opentelemetry.android.agent.setSlowRenderingDetectionPollInterval
 import io.opentelemetry.android.config.OtelRumConfig
 import io.opentelemetry.android.features.diskbuffering.DiskBufferingConfiguration
 import io.opentelemetry.api.common.AttributeKey.stringKey
@@ -20,8 +19,8 @@ import io.opentelemetry.api.trace.Tracer
 import io.opentelemetry.exporter.otlp.http.logs.OtlpHttpLogRecordExporter
 import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter
 import io.opentelemetry.sdk.logs.internal.SdkEventLoggerProvider
-import java.time.Duration
-import kotlin.math.log
+import io.opentelemetry.sdk.resources.Resource
+import java.util.UUID
 
 const val TAG = "otel.demo"
 
@@ -31,38 +30,84 @@ class OtelDemoApplication : Application() {
         super.onCreate()
 
         Log.i(TAG, "Initializing the opentelemetry-android-agent")
-        val diskBufferingConfig =
-            DiskBufferingConfiguration.builder()
-                .setEnabled(true)
-                .setMaxCacheSize(10_000_000)
-                .build()
-        val config =
-            OtelRumConfig()
-                .setGlobalAttributes(Attributes.of(stringKey("toolkit"), "jetpack compose"))
-                .setDiskBufferingConfiguration(diskBufferingConfig)
 
-        // 10.0.2.2 is apparently a special binding to the host running the emulator
-        val spansIngestUrl = "http://10.0.2.2:4318/v1/traces"
-        val logsIngestUrl = "http://10.0.2.2:4318/v1/logs"
-        val otelRumBuilder: OpenTelemetryRumBuilder =
-            OpenTelemetryRum.builder(this, config)
-                .addSpanExporterCustomizer {
-                    OtlpHttpSpanExporter.builder()
-                        .setEndpoint(spansIngestUrl)
-                        .build()
-                }
-                .addLogRecordExporterCustomizer {
-                    OtlpHttpLogRecordExporter.builder()
-                        .setEndpoint(logsIngestUrl)
-                        .build()
-                }
         try {
-            rum = otelRumBuilder.build()
+            val customRumBuilder = FakeOpenTelemetryRumBuilder.getDefault(this)
+            customRumBuilder.mergeResource(
+                Resource.builder().put("custom_id", UUID.randomUUID().toString()).build()
+            )
+
+            rum = AndroidAgent.createOpenTelemetryRum(
+                application = this,
+                rumBuilder = customRumBuilder
+            )
             Log.d(TAG, "RUM session started: " + rum!!.rumSessionId)
         } catch (e: Exception) {
             Log.e(TAG, "Oh no!", e)
         }
     }
+
+    object AndroidAgent {
+        fun createOpenTelemetryRum(
+            application: Application,
+            appName: String? = null,
+            appVersion: String? = null,
+            endpointConfig: EndpointConfig = EndpointConfig(),
+            rumBuilder: OpenTelemetryRumBuilder = FakeOpenTelemetryRumBuilder.getDefault(application)
+        ): OpenTelemetryRum {
+            appName?.apply {
+                rumBuilder.mergeResource(
+                    Resource.builder().put("service.name", this).build()
+                )
+            }
+
+            appVersion?.apply {
+                rumBuilder.mergeResource(
+                    Resource.builder().put("service.version", this).build()
+                )
+            }
+
+            endpointConfig.apply {
+                spanIngestUrl?.apply {
+                    rumBuilder.addSpanExporterCustomizer {
+                        OtlpHttpSpanExporter.builder()
+                            .setEndpoint(this)
+                            .build()
+                    }
+                }
+                logIngestUrl?.apply {
+                    rumBuilder.addLogRecordExporterCustomizer {
+                        OtlpHttpLogRecordExporter.builder()
+                            .setEndpoint(this)
+                            .build()
+                    }
+                }
+            }
+
+            return rumBuilder.build()
+        }
+    }
+
+    object FakeOpenTelemetryRumBuilder {
+        fun getDefault(application: Application): OpenTelemetryRumBuilder {
+            val diskBufferingConfig =
+                DiskBufferingConfiguration.builder()
+                    .setEnabled(true)
+                    .setMaxCacheSize(10_000_000)
+                    .build()
+            val config =
+                OtelRumConfig()
+                    .setGlobalAttributes(Attributes.of(stringKey("toolkit"), "jetpack compose"))
+                    .setDiskBufferingConfiguration(diskBufferingConfig)
+
+            return OpenTelemetryRum.builder(application, config)
+        }
+    }
+
+    data class EndpointConfig(
+        val spanIngestUrl: String? = "http://10.0.2.2:4318/v1/traces",
+        val logIngestUrl: String? = "http://10.0.2.2:4318/v1/logs"
+    )
 
     companion object {
         var rum: OpenTelemetryRum? = null
